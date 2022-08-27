a4500e0463d8ea4e1cbed4355a3bd665d0319c4c
liudong <1076624325@qq.com>
2022-08-27 18:59:33 +0800

    Add: validate

---
+++ utils/tools.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"errors"
+	"github.com/go-playground/validator/v10"
+	"strings"
+)
+
+// 校验数据
+func Validate(struc interface{}) error {
+	v := validator.New()
+	err := v.Struct(struc)
+	if err != nil {
+		msg := "Invalid parameter: "
+		es := err.(validator.ValidationErrors)
+		errFields := []string{}
+		for _, e := range es {
+			errFields = append(errFields, e.Field())
+		}
+		// 拼接不符合要求的字段
+		msg += strings.Join(errFields, ",")
+		return errors.New(msg)
+	}
+	return nil
+}
