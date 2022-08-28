d5fa9bbd9a8216a332ad2d1c7c88b2c92ca9b846
liudong <1076624325@qq.com>
2022-08-28 18:55:45 +0800

    Add: 修改 validate

--- utils/tools.go
+++ utils/tools.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"github.com/go-playground/validator/v10"
 	"strings"
 )
@@ -15,7 +16,7 @@ func Validate(struc interface{}) error {
 		es := err.(validator.ValidationErrors)
 		errFields := []string{}
 		for _, e := range es {
-			errFields = append(errFields, e.Field())
+			errFields = append(errFields, fmt.Sprintf("%v(%v)", e.StructField(), e.ActualTag()))
 		}
 		// 拼接不符合要求的字段
 		msg += strings.Join(errFields, ",")
